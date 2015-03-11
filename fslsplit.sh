b98f37569cd9faf4aefa01ccfb7b63af7ef70046
Allison Jack <allison.e.jack@gmail.com>
2015-03-11 11:43:41 -0400

    Create fslsplit.sh

---
+++ fslsplit.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+STUDYDIR=/scratch/aj349/BIOPOINT_CRBLM
+PRESTATS=$STUDYDIR/ADULT/ANALYSIS/PRESTATS_SUIT
+
+IDLIST=$STUDYDIR/IDs.txt
+
+  while read -r ID || [[ -n ''$ID'' ]]; do
+
+    FEATDIR=$PRESTATS/${ID}.feat
+    SUITDIR=$STUDYDIR/ADULT/SUIT/$ID
+    FILTDIR=$SUITDIR/FILTERED_FX
+
+    mkdir -p $FILTDIR
+    mkdir -p $SUITDIR
+
+    INPUT=$FEATDIR/filtered_func_data.nii.gz
+    MAT=$FEATDIR/reg/example_func2highres.mat
+    TEMP=$SUITDIR/filtered_func_highres_temp.nii.gz
+    REF=$FEATDIR/reg/highres.nii.gz
+
+    fslsplit $INPUT $FILTDIR/filtered_func_data_ -t
+
+  done < $IDLIST
