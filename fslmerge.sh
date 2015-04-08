12c7b219ddf007b384930dbef9fcd990ca726e54
Allison Jack <allison.e.jack@gmail.com>
2015-04-08 13:33:09 -0400

    Create fslmerge.sh

---
+++ fslmerge.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SCRATCH=/scratch/aj349/BIOPOINT_CRBLM
+LVL1=$SCRATCH/ADULT/ANALYSIS/PPI_LVL1_SUIT
+IDLIST=$SCRATCH/LVL3_SUIT_motion_matched/AC_matched.txt
+
+  for ROI in iRpSTS LpSTS; do
+
+    LVL3=$SCRATCH/${ROI}_PPI_LVL3_SUIT_motion_matched
+    mkdir -p $LVL3
+
+      while read -r ID || [[ -n "$ID" ]]; do
+
+        SUBJDIR=$LVL1/$ROI/${ID}.feat/stats
+
+        printf "$SUBJDIR/cope3.nii.gz " >> copein_${ROI}_tmp.txt
+        printf "$SUBJDIR/varcope3.nii.gz " >> varcopein_${ROI}_tmp.txt
+
+      done < $IDLIST
+
+    COPEIN=$(cat copein_${ROI}_tmp.txt)
+    VARCOPEIN=$(cat varcopein_${ROI}_tmp.txt)
+
+    fslmerge -t $LVL3/AC_cope3_4D.nii.gz $COPEIN
+    fslmerge -t $LVL3/AC_varcope3_4D.nii.gz $VARCOPEIN
+
+  done
+
+rm *tmp*
