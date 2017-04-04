20d6aff96a1294b7ab7eb9c2a28c6cc1b552387b
Allison Jack <allison.e.jack@gmail.com>
2017-04-04 16:47:15 -0400

    Update fslsplit.sh

--- fslsplit.sh
+++ fslsplit.sh
@@ -1,23 +1,20 @@
 #!bin/bash
 
 STUDYDIR=/PATH_TO_YOUR_DIRECTORY/USERNAME/STUDYNAME
-PRESTATS=$STUDYDIR/ADULT/ANALYSIS/PRESTATS_SUIT
+PRESTATS=$STUDYDIR/ANALYSIS/PRESTATS_SUIT
 
 IDLIST=$STUDYDIR/IDs.txt
 
   while read -r ID || [[ -n ''$ID'' ]]; do
 
     FEATDIR=$PRESTATS/${ID}.feat
-    SUITDIR=$STUDYDIR/ADULT/SUIT/$ID
+    SUITDIR=$STUDYDIR/SUIT/$ID
     FILTDIR=$SUITDIR/FILTERED_FX
 
     mkdir -p $FILTDIR
     mkdir -p $SUITDIR
 
     INPUT=$FEATDIR/filtered_func_data.nii.gz
-    MAT=$FEATDIR/reg/example_func2highres.mat
-    TEMP=$SUITDIR/filtered_func_highres_temp.nii.gz
-    REF=$FEATDIR/reg/highres.nii.gz
 
     fslsplit $INPUT $FILTDIR/filtered_func_data_ -t
 
